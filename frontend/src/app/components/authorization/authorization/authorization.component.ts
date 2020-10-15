3e4a6bf3fe7757cdbdd121ee292f8932d897d965
barnabas103002 <barnabas.ebadan@exlservice.com>
2020-10-15 08:54:04 -0700

    - implemented date range validation - improve alert message toaster code - disable save button when the form is not in valid state

--- frontend/src/app/components/authorization/authorization/authorization.component.ts
+++ frontend/src/app/components/authorization/authorization/authorization.component.ts
@@ -9,6 +9,7 @@ import { User } from 'src/app/models/user';
 import { Member } from 'src/app/models/member';
 import { Transaction } from 'src/app/models/auth/transaction';
 import { FormControl, Validators } from '@angular/forms';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-authorization',
@@ -26,6 +27,7 @@ export class AuthorizationComponent implements OnInit {
   infoMessage: string;
   memberUid: number;
   contactMethods = [ContactMethod.EMAIL, ContactMethod.FAX, ContactMethod.PHONE, ContactMethod.VIDEO];
+  isValidDateRange = false;
 
   constructor(private userService: UserService,
               private authorizationService: AuthorizationService,
@@ -55,27 +57,46 @@ export class AuthorizationComponent implements OnInit {
       this.errorMessage = 'User must sign-in to iniatite authorization for a member';
       return;
     }
+    // check for date valid date range
+    this.isValidDateRange = this.validateDates(authorization.dateValidFrom, authorization.dateValidTo);
 
+    console.log('Valid From Date:', authorization.dateValidFrom + 'and Valid To Date:', authorization.dateValidTo);
     const promise = new Promise((resolve, reject) => {
-     setTimeout(() => {
-      const transaction = new Transaction();
+    if (this.isValidDateRange) {
+      setTimeout(() => {
+        const transaction = new Transaction();
 
-      transaction.memberInternalId = this.memberUid;
-      transaction.authorization = authorization;
-      transaction.createdBy = this.currentUser.id;
+        transaction.memberInternalId = this.memberUid;
+        transaction.authorization = authorization;
+        transaction.createdBy = this.currentUser.id;
 
-      this.authorizationService.initiate(transaction).subscribe(data => {
-        this.infoMessage = 'Successfully Initiated Authorization';
-      }, err => {
-        this.errorMessage = 'Unexpected error occurred while trying to initiate Authorization';
-      });
-      resolve();
-     }, 1500);
-    }); // end of promise
+        this.authorizationService.initiate(transaction).subscribe(data => {
+            this.infoMessage = 'Successfully Initiated Authorization';
+          }, err => {
+            this.errorMessage = 'Unexpected error occurred while trying to initiate Authorization';
+          });
+
+        resolve();
+
+      }, 1500);
+    }
+    }); // end of promise instantiation
 
     this.getMemberUid();
-    return promise;
 
+    return promise;
+  }
+
+  validateDates(date1: Date, date2: Date) {
+    this.isValidDateRange = true;
+    if ((date1 && date2) && (date2 < date1)) {
+      this.errorMessage = '"Valid To" date should not be greater than "Valid From" date';
+      this.isValidDateRange = false;
+    } else if (!date1 && date2) {
+      this.errorMessage = 'Invalid date range between "Valid To" and "Valid From" dates';
+      this.isValidDateRange = false;
+    }
+    return this.isValidDateRange;
   }
 
 }
